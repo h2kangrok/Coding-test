faca182e49a714bb09b6e66ea81ac5cdf1a549b0
KangRok Yoon <ykr0919@naver.com>
2023-11-04 17:47:59 +0900

    Initial Commit

---
+++ Coding-test/1316.swift
@@ -0,0 +1,29 @@
+//
+//  1316.swift
+//  Coding-test
+//
+//  Created by 윤강록 on 11/4/23.
+//
+
+import Foundation
+
+//let n = Int(readLine()!)!
+//var count = 0
+//for _ in 0..<n {
+//    let word = readLine()!
+//    var arr: [Character] = []
+//    var token = false
+//    for i in word {
+//        if !arr.contains(i) {
+//            arr.append(i)
+//        } else {
+//            if arr.last != i {
+//                token = true
+//            }
+//        }
+//    }
+//    if !token {
+//        count += 1
+//    }
+//}
+//print("\(count)")
---
+++ Coding-test/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Coding-test
+//
+//  Created by 윤강록 on 11/4/23.
+//
+
+import Foundation
+
+print("Hello, World!")
+
