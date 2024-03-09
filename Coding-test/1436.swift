f0092b471746090ea48e6c5a718f1d8014aed102
KangRok Yoon <ykr0919@naver.com>
2024-03-09 22:23:46 +0900

    1436 commit

---
+++ Coding-test/1436.swift
@@ -0,0 +1,37 @@
+//
+//  1436.swift
+//  Coding-test
+//
+//  Created by 윤강록 on 3/9/24.
+//
+
+//import Foundation
+//
+//extension Int {
+//    func isDevilNumber() -> Bool {
+//        var number = self
+//        while number >= 666{
+//            if number % 1000 == 666 {
+//                return true
+//            }
+//            number /= 10
+//        }
+//        return false
+//    }
+//}
+//
+//let n = Int(readLine()!)!
+//var index = 0
+//
+//var devilNumbers: [Int] = []
+//var number = 666
+//
+//while index < n {
+//    if number.isDevilNumber() {
+//        devilNumbers.append(number)
+//        index += 1
+//    }
+//    number += 1
+//}
+//
+//print(devilNumbers[n-1])
