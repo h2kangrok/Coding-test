7a7555f261d4215d6fd71ead7ddeaa46ff143556
KangRok Yoon <ykr0919@naver.com>
2024-03-18 21:17:44 +0900

    1929 commit

---
+++ Coding-test/1929.swift
@@ -0,0 +1,31 @@
+//
+//  1929.swift
+//  Coding-test
+//
+//  Created by 윤강록 on 3/18/24.
+//
+
+//import Foundation
+//
+//let input = readLine()!.split(separator: " ").map { Int($0)! }
+//let m = input[0]
+//let n = input[1]
+//
+//var isPrimeNumber = [Bool](repeating: true, count: n + 1)
+//isPrimeNumber[1] = false
+//
+//for i in 2..<Int(sqrt(Double(n)) + 1) {
+//    if isPrimeNumber[i] {
+//        var j = 2
+//        while i * j <= n {
+//            isPrimeNumber[i * j] = false
+//            j += 1
+//        }
+//    }
+//}
+//
+//for i in m...n {
+//    if isPrimeNumber[i] {
+//        print(i)
+//    }
+//}
--- README.md
+++ README.md
@@ -33,6 +33,7 @@
 [1676](https://www.acmicpc.net/problem/1676)
 [1874](https://www.acmicpc.net/problem/1676)
 [1920](https://www.acmicpc.net/problem/1920)
+[1929](https://www.acmicpc.net/problem/1920)
 
 ## 문제 풀이 정리할 때 포함되어야 하는 내용들
 
