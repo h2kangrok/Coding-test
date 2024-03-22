b9b243fea29bc7d13fdde0119c5d662dbcaa9c60
KangRok Yoon <ykr0919@naver.com>
2024-03-22 23:57:11 +0900

    2108 first commit

---
+++ Coding-test/2108.swift
@@ -0,0 +1,36 @@
+//
+//  2108.swift
+//  Coding-test
+//
+//  Created by 윤강록 on 3/22/24.
+//
+
+//import Foundation
+//
+//var array:[Int] = []
+//var sum = 0
+//
+//var count = Int(readLine()!)!
+//
+//for _ in 0..<count {
+//    var n = Int(readLine()!)!
+//        sum += n
+//    array.append(n)
+//}
+// 
+//print(String(format: "%.1f", sum/count), solution(_arr: array), solution1(_arr: array), array.max()!-array.min()!)
+//
+//
+//func solution(_arr:[Int])-> Int {
+//    var tmp = array
+//    tmp.sort()
+//    return tmp[tmp.count/2]
+//}
+//
+//
+//func solution1(_arr:[Int])-> Int {
+//    var tmp = array
+//    tmp.sort()
+//    return tmp[1]
+//}
+//
