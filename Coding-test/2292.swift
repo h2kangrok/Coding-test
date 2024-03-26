addffb2cbf66a32b96984d4b377eabfbc7c3e583
KangRok Yoon <ykr0919@naver.com>
2024-03-26 23:57:22 +0900

    2292 commit

---
+++ Coding-test/2292.swift
@@ -0,0 +1,17 @@
+//
+//  2292.swift
+//  Coding-test
+//
+//  Created by 윤강록 on 3/26/24.
+//
+
+//import Foundation
+//
+//let n = Int(readLine()!)!
+//var i = 1
+//var start = 1
+//while start < n {
+//    start += (6 * i)
+//    i += 1
+//}
+//print(i)
--- README.md
+++ README.md
@@ -39,6 +39,7 @@
 [2108](https://www.acmicpc.net/problem/2108)
 [2164](https://www.acmicpc.net/problem/2164)
 [2231](https://www.acmicpc.net/problem/2231)
+[2292](https://www.acmicpc.net/problem/2292)
 
 ## 문제 풀이 정리할 때 포함되어야 하는 내용들
 
