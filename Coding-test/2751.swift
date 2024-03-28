ad9c7e8b2fe78ef92bc08c1eee1ac37d4fe3af84
KangRok Yoon <ykr0919@naver.com>
2024-03-28 18:35:20 +0900

    2751 commit

---
+++ Coding-test/2751.swift
@@ -0,0 +1,46 @@
+//
+//  2751.swift
+//  Coding-test
+//
+//  Created by 윤강록 on 3/28/24.
+//
+
+import Foundation
+
+//var arr:[Int] = []
+//let n = Int(readLine()!)!
+//
+//for _ in 0..<n {
+//    arr.append(Int(readLine()!)!)
+//}
+//arr.sort()
+//
+//print(arr.sorted().map { String($0) }.joined(separator: "\n"))
+
+//for i in 0..<n {
+//    print("\(arr[i])")
+//}
+//func insertionSort(_ array: inout [Int]) -> [Int] {
+//    for stand in 1..<array.count {
+//        for index in stride(from: stand, to: 0, by: -1) {
+//            if array[index] < array[index - 1] {
+//                array.swapAt(index, index - 1)
+//            } else {
+//                break
+//            }
+//        }
+//    }
+//    return array
+//}
+//
+//let n = Int(readLine()!)!
+//var array:[Int] = []
+//
+//for _ in 0..<n {
+//    array.append(Int(readLine()!)!)
+//}
+//let sortedArray = insertionSort(&array)
+//for sortedElement in sortedArray {
+//    print(sortedElement)
+//}
+
--- Coding-test/main.swift
+++ Coding-test/main.swift
@@ -4,4 +4,5 @@
 //
 //  Created by 윤강록 on 11/4/23.
 
+import Foundation
 
