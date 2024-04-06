51ab3648c0e38eeb108fd5a2b03513ac40f9e553
KangRok Yoon <ykr0919@naver.com>
2024-04-06 17:04:32 +0900

    10773 commit

---
+++ Coding-test/10773.swift
@@ -0,0 +1,28 @@
+//
+//  10773.swift
+//  Coding-test
+//
+//  Created by 윤강록 on 4/6/24.
+//
+
+//import Foundation
+//
+//var n:[Int] = []
+//// 우선 정수 k가 주어짐
+//let k = Int(readLine()!)!
+//
+//// for문을 10만번?? -> 넘을꺼 같은데
+//// 입력을 받을 때마다 바로 바로 처리 해줘야할꺼 같은데
+//for _ in 0..<k {
+//    let input = Int(readLine()!)!
+//    if input != 0 {
+//        n.append(input)
+//    } else {
+//        n.removeLast()
+//    }
+//    
+//}
+//print(n.reduce(0, +))
+//
+//// 어짜피 초기값으로 나오니깐 0이 나오겠네
+////n.isEmpty ? print(0) : print(n.reduce(0, +))
--- README.md
+++ README.md
@@ -43,6 +43,7 @@
 [4949](https://www.acmicpc.net/problem/4949)
 [7568](https://www.acmicpc.net/problem/7568)
 [9012](https://www.acmicpc.net/problem/9012)
+[10773](https://www.acmicpc.net/problem/10773)
 
 ## 문제 풀이 정리할 때 포함되어야 하는 내용들
 
