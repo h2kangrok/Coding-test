b5179cec8c30f33c7b4d72cebe38ae073327d956
KangRok Yoon <ykr0919@naver.com>
2024-04-10 19:18:44 +0900

    10989 commit

---
+++ Coding-test/10989.swift
@@ -0,0 +1,41 @@
+//
+//  10989.swift
+//  Coding-test
+//
+//  Created by 윤강록 on 4/10/24.
+//
+
+// MARK: CountingSort
+
+//import Foundation
+//
+//var countingNums = [Int](repeating: 0, count: 10001)
+//let n = Int(readLine()!)!
+//
+//for _ in 0..<n {
+//    countingNums[Int(readLine()!)!] += 1
+//}
+//
+//var output = ""
+//
+//for i in 1...10000 {
+//    output += String(repeating: "\(i)\n", count: countingNums[i])
+//}
+//
+//print(output)
+
+
+
+
+// MARK: 시간초과
+//let n = Int(readLine()!)!
+//
+//var arr:[Int] = []
+//
+//for _ in 0..<n {
+//    arr.append(Int(readLine()!)!)
+//}
+//arr.sort()
+//
+//print(arr.map { String($0)}.joined(separator: "\n"))
+
--- README.md
+++ README.md
@@ -49,6 +49,7 @@
 [10828](https://www.acmicpc.net/problem/10828)
 [10845](https://www.acmicpc.net/problem/10845)
 [10866](https://www.acmicpc.net/problem/10866)
+[10989](https://www.acmicpc.net/problem/10989)
 
 ## 문제 풀이 정리할 때 포함되어야 하는 내용들
 
