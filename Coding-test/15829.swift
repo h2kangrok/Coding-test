fd0e4b96108e90559acf90727bf0d1806c63850b
KangRok Yoon <ykr0919@naver.com>
2024-04-13 14:53:25 +0900

    15829 commit

---
+++ Coding-test/15829.swift
@@ -0,0 +1,51 @@
+//
+//  15829.swift
+//  Coding-test
+//
+//  Created by 윤강록 on 4/13/24.
+//
+
+//import Foundation
+//
+//let mod = 1234567891
+//let n = Int(readLine()!)!
+//let input = Array(readLine()!).map { Int(exactly: $0.asciiValue!)! - 96}
+//var hash = 0
+//var m = 1
+//for i in input {
+//    hash = (hash + i*m) % mod
+//    m = (m*31) % mod
+////    hash += i*Int(pow(31.0, Double(m))) % mod
+////    m += 1
+////
+//}
+//print(hash)
+
+//// 문자열 길이
+//let n = Int(readLine()!)!
+//
+//// 문자열 배열
+//var charArr:[Character] = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+//var numArr = Array(1...26)
+//var result = 0
+//
+//// 여기서 굳이 문자열 자리수 제안을 안해도 괜찮을 꺼 같음
+//var input = readLine()!
+//for num in input {
+//    for char in charArr {
+//        if num == char {
+//
+//        }
+//    }
+//}
+//
+//for i in 0..<n {
+//
+//}
+//let str = "Hello"
+//let characters = Array(str)
+//
+//for char in characters {
+//    print(char)
+//}
+
--- README.md
+++ README.md
@@ -54,6 +54,7 @@
 [11650](https://www.acmicpc.net/problem/11650)
 [11651](https://www.acmicpc.net/problem/11651)
 [11866](https://www.acmicpc.net/problem/11866)
+[15829](https://www.acmicpc.net/problem/15829)
 
 
 ## 문제 풀이 정리할 때 포함되어야 하는 내용들
