a19535de27675ea08a47c9d20764c6e5dedefa5e
KangRok Yoon <ykr0919@naver.com>
2024-05-11 20:51:53 +0900

    2941 commit

---
+++ Coding-test/2941(크로아티아 알파벳).swift
@@ -0,0 +1,19 @@
+//
+//  2941(크로아티아 알파벳).swift
+//  Coding-test
+//
+//  Created by 윤강록 on 5/11/24.
+//
+
+//import Foundation
+//
+//// 변경된 크로아티아 언어가 나오면 if- else 구문으로 없애주기
+//
+//// 입력받고
+//var input = readLine()!
+//
+//["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="].forEach {
+//    input = input.replacingOccurrences(of: $0, with: "1")
+//}
+//
+//print(input.count)
